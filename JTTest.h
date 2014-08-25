37aa748e96bbdfd37bf248b5129e8d5e0709aff1
Joshua  Tan <joshuatan@Joshuas-MacBook-Pro.local>
2014-08-25 21:51:09 +0800

    added a test class

---
+++ JTTest.h
@@ -0,0 +1,13 @@
+//
+//  JTTest.h
+//  Git Test
+//
+//  Created by Joshua  Tan on 25/8/14.
+//  Copyright (c) 2014 Tytan Apps. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JTTest : NSObject
+
+@end
---
+++ JTTest.m
@@ -0,0 +1,13 @@
+//
+//  JTTest.m
+//  Git Test
+//
+//  Created by Joshua  Tan on 25/8/14.
+//  Copyright (c) 2014 Tytan Apps. All rights reserved.
+//
+
+#import "JTTest.h"
+
+@implementation JTTest
+
+@end
